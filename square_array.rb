1ee9d0502ce4bb23b7d6ea20d4dab81ec07f2f82
Jesse Murphy <jessemurphy1982@gmail.com>
2020-07-13 19:52:24 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -5,8 +5,10 @@
 def square_array(array)
   new_numbers = []
   array.each do |numbers|
-   new_numbers<<numbers * numbers
+   new_numbers<< numbers * numbers
   end
+  
+  new_numbers
   
 end
     
